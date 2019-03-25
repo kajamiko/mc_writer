473fcbb347b63933e20b42f0ea9f0b7b3e71793f
kajamiko <kajaths@gmail.com>
2019-03-25 22:50:46 +0000

    Most of known bugs fixed

---
+++ js/expand_desc.js
@@ -0,0 +1,41 @@
+
+var exp, par;
+exp = ".exp-";
+par = ".par-"
+
+// var i;
+
+// function expand_paragraph(i) {
+//     var paragraph = document.querySelector(par + i)
+//     if (paragraph.style.display === "none") {
+//         paragraph.style.display = "block";
+//       } else {
+//         paragraph.style.display = "none";
+//       };
+// }
+
+// for(i = 0; i<2; i++ ){
+//     document.querySelector(exp + i).addEventListener('click', function() {
+//         expand_paragraph(i);
+// });
+// }
+
+
+    document.querySelector(exp + "0").addEventListener('click', function() {
+        var paragraph = document.querySelector(par + "0")
+        if (paragraph.style.display === "none") {
+                        paragraph.style.display = "block";
+                      } else {
+                        paragraph.style.display = "none";
+                      }
+    });
+    document.querySelector(exp + "1").addEventListener('click', function() {
+        var paragraph = document.querySelector(par + "1")
+        if (paragraph.style.display === "none") {
+                        paragraph.style.display = "block";
+                      } else {
+                        paragraph.style.display = "none";
+                      }
+    });
+
+   
--- js/resume.js
+++ js/resume.js
@@ -15,8 +15,11 @@
     }
   });
 
+   
+
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function() { 
+   
     $('.navbar-collapse').collapse('hide');
   });
 
